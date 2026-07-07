ea107460f328dc5056672a230eaf45a91ae9da2d
Diego Grajeda <grajedadiego1@gmail.com>
2026-07-07 10:37:04 -0400

    primer commit

---
+++ src/App.css
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-zinc-950 text-white">
+      <h1 className="text-3xl font-bold tracking-tight">
+        StockCore v4 + React Compiler 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
